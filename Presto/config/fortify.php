dd2e3a16eaf35c7750b34e5ce39ae8a843462ed4
Giuseppe Galluzzi <galluzzigiuseppe99@gmail.com>
2021-04-27 15:26:20 +0200

    conflitto risolto spero

--- Presto/config/fortify.php
+++ Presto/config/fortify.php
@@ -135,10 +135,7 @@ return [
         Features::registration(),
         // Features::resetPasswords(),
         // Features::emailVerification(),
-<<<<<<< HEAD
         // // Features::emailVerification(),
-=======
->>>>>>> 5301472ae99edde112d9cd78806e20c149c41e78
         // Features::updateProfileInformation(),
         // Features::updatePasswords(),
         // Features::twoFactorAuthentication([
