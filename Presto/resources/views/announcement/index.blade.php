7fcb4d933746132a68e28d3a48cd9d7549561be1
Piero Colapietro <pierocolapietro88@gmail.com>
2021-04-28 09:29:47 +0200

    ok categories

--- Presto/resources/views/announcement/index.blade.php
+++ Presto/resources/views/announcement/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <div class="container">
         @if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
         @endif
         <div class="row">
             <div class="col-12 col-md-6 offset-md-3">
@@ -12,35 +12,42 @@
             <div class="row">
                 <div class="col-12 col-md-3">
                     @foreach ($announcements as $announcement)
-                    <div class="card">
-                    @if ($announcement->img)
-                        <img src="{{Storage::url($announcement->img)}}" alt="{{$announcement->title}}">
-                        @else
-                       <img src="https://via.placeholder.com/150" alt="{{$announcement->title}}"> 
+                        <div class="card">
+                            <div class="row">
+                                <div class="card-img">
+                                    @if ($announcement->img)
+                                        <img src="{{ Storage::url($announcement->img) }}"
+                                            alt="{{ $announcement->title }}">
+                                    @else
+                                        <img src="https://via.placeholder.com/150" alt="{{ $announcement->title }}">
+                                </div>
                     @endif
-                        <div class="card-body">
-                            <h3>{{$announcement->title}}</h3>
-                            <p>{{$announcement->description}}</p>
-                            <p>{{$announcement->price}}</p>
-                            <p>Annuncio di: {{$announcement->user->name}}</p>
+                    <div class="card-body">
 
-                            <p>Creato il: {{$announcement->created_at->format('d-m-Y- H:i:s')}}</p>
-                            <div> Categoria:<a href="{{route('announcement.cat', [
-                                $announcement->category->name,
-                                $announcement->category->id
-                            ])}}"> 
-                            {{$announcement->category->name}}</a></div>
-                          {{-- <a href="#" class="btn btn-primary">Vai al dettaglio</a> --}}
+                        <div class="card-title">
+                            <h3>{{ $announcement->title }}</h3>
                         </div>
-                      </div>
-                    @endforeach
+                        <div class="card-description">
+                            <p>{{ $announcement->description }}</p>
+                        </div>
+                        <div class="card-price">
+                            <p>{{ $announcement->price }}</p>
+                        </div>
+                        <div class="card-auth">
+                            <p>Annuncio di: {{ $announcement->user->name }}</p>
+                        </div>
+
+                        <p>Creato il: {{ $announcement->created_at->format('d-m-Y- H:i:s') }}</p>
+                        <div> Categoria: <a
+                                href="{{ route('announcement.cat', [$announcement->category->name, $announcement->category->id]) }}">
+                                {{ $announcement->category->name }}</a></div>
+                        {{-- <a href="#" class="btn btn-primary">Vai al dettaglio</a> --}}
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
+    </div>
+    </div>
 </x-layout>
-
-
-
-
-
--- Presto/resources/views/welcome.blade.php
+++ Presto/resources/views/welcome.blade.php
@@ -1,131 +1,138 @@
 <x-layout>
-    <h1>Benvenuto in Presto.it</h1>
+    <h1 class="col-12 text-center my-5"> <b> Benvenuto in Presto.it </b> </h1>
 
-    {{-- <div class="row">
+    <div class="col-12 text-center my-5">
+        <h2>Esplora le nostre categorie</h2>
+    </div>
+    <div class="row justify-content-center">
         @foreach ($categories as $category)
-            <div class="col-12 col-md-1 ms-auto my-5">
-                <form action="{{route('announcement.cat')}}" method="POST">
-                <input type="text" value="{{$category->id}}">{{$category->name}}</a>
-                </form>        
-            </div>
-
-                @endforeach --}}
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-8 offset-md-2 mt-5 mb-5">
-                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
-                            class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
+            <div class="col-12 col-md-2 text-center ms-5">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <h5 class="card-title text-center my-4"><i class="fs-4 fas fa-store"></i> {{ $category->name }}</h5>
+                        <button class="btn btn-primary text-center"> <a href="{{route('announcement.cat',[
+                            $category->name,
+                            $category->id
+                          ]) }}">Cerca in {{ $category->name }}</a></button>
                     </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://via.placeholder.com/200" class="d-block w-100 img-thumbnail" alt="">
-                            <div class="carousel-caption d-none d-md-block">
-                                
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://via.placeholder.com/200" class="d-block w-100 img-thumbnail" alt="">
-                            <div class="carousel-caption d-none d-md-block">
-                                
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://via.placeholder.com/200" class="d-block w-100 img-thumbnail" alt="">
-                            <div class="carousel-caption d-none d-md-block">
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
-        </div>
-    </div>
+        @endforeach
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-8 offset-md-2 mt-5 mb-5">
-                <div class="accordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button accordion" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
-                                aria-controls="collapseOne">
-                                Accordion 3
-                            </button>
-                        </h2>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is hidden by default,
-                                until the collapse plugin adds the appropriate classes that we use to style each
-                                element. These classes control the overall appearance, as well as the showing and
-                                hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                our default variables. It's also worth noting that just about any HTML can go within
-                                the <code>.accordion-body</code>, though the transition does limit overflow.
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 mt-5 mb-5">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                                class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner text-center">
+                            <div class="carousel-item active">
+                                <img src="{{Storage::url('img/deal1.png')}}" class="img-fluid" alt="">
+                                <div class="carousel-caption d-none d-md-block text-img">
+                                <h3>Cerca tra migliaia di annunci e inserisci i tuoi, ovunque tu sia.</h3>
+                            </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{Storage::url('img/deal2.png')}}" class="img-fluid" alt="">
+                                <div class="carousel-caption d-none d-md-block text-img">
+                                <h3>Dai al tuo usato una seconda occasione: vendi quello che non usi più.</h3>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{Storage::url('img/deal3.png')}}" class="img-fluid" alt="">
+                                <div class="carousel-caption d-none d-md-block text-img">
+                                <h3>Cerco l'oggetto più vicino a te e incontra il venditore.</h3>
                             </div>
                         </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button accordion collapsed" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
-                                aria-controls="collapseTwo">
-                                Accordion 2
-                            </button>
-                        </h2>
-                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default,
-                                until the collapse plugin adds the appropriate classes that we use to style each
-                                element. These classes control the overall appearance, as well as the showing and
-                                hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                our default variables. It's also worth noting that just about any HTML can go within
-                                the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
                         </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button accordion collapsed" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
-                                aria-controls="collapseThree">
-                                Accordion 1
-                            </button>
-                        </h2>
-                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default,
-                                until the collapse plugin adds the appropriate classes that we use to style each
-                                element. These classes control the overall appearance, as well as the showing and
-                                hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                                our default variables. It's also worth noting that just about any HTML can go within
-                                the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    
-</header>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-8 offset-md-2 mt-5 mb-5">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button accordion" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Accordion 3
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>This is the first item's accordion body.</strong> It is hidden by default,
+                                    until the collapse plugin adds the appropriate classes that we use to style each
+                                    element. These classes control the overall appearance, as well as the showing and
+                                    hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                                    our default variables. It's also worth noting that just about any HTML can go within
+                                    the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button accordion collapsed" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                                    aria-controls="collapseTwo">
+                                    Accordion 2
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>This is the second item's accordion body.</strong> It is hidden by default,
+                                    until the collapse plugin adds the appropriate classes that we use to style each
+                                    element. These classes control the overall appearance, as well as the showing and
+                                    hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                                    our default variables. It's also worth noting that just about any HTML can go within
+                                    the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingThree">
+                                <button class="accordion-button accordion collapsed" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
+                                    aria-controls="collapseThree">
+                                    Accordion 1
+                                </button>
+                            </h2>
+                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                                    until the collapse plugin adds the appropriate classes that we use to style each
+                                    element. These classes control the overall appearance, as well as the showing and
+                                    hiding via CSS transitions. You can modify any of this with custom CSS or overriding
+                                    our default variables. It's also worth noting that just about any HTML can go within
+                                    the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </header>
 </x-layout>
